2ca61f6a9f5e4931c3cb8395e710aededf673264
Svilen Genchev <svileng@gmail.com>
2021-02-20 13:28:32 -0800

    implement prices

---
+++ assets/js/render_prices.js
@@ -0,0 +1,55 @@
+
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+var theUrl = "https://script.google.com/macros/s/AKfycbwHAwGpRWQk4N_MzbCkJ7u1S6ki3D7SAfNp1KjuNGpxbd1kAyBi1_NmKQ/exec";
+
+var locationParameter = getUrlVars()["location"];
+if (typeof locationParameter !== "undefined" && (typeof locationParameter !== "object" || !locationParameter)) {
+    theUrl += '?location=' + locationParameter;
+}
+
+// var theUrl = "test/data.js";
+
+$.ajax({
+    type: "GET",
+    url: theUrl,
+    dataType: "text",
+    success: function (response) {
+        eval(response);
+        var html = '';
+        var count = 0;
+        
+        $.each(data, function (index, scheduleEntry) {
+            ++count;
+            console.log(count);
+            var name = scheduleEntry['name'];
+            var price = scheduleEntry['price'];
+            var bcgrColor = 'black';
+            if (count%2 == 0 ) {
+                bcgrColor = 'grey';
+            }
+            html += '<div class="row" style="background-color: ' + bcgrColor+ '">';
+                html += '<div class="col-sm" style="border-right: 1px solid white; border-bottom: 1px solid white;">';
+                html += name;
+                html += '</div>';
+                html += '<div class="col-sm" style="border-left: 1px solid white;  border-bottom: 1px solid white;">';
+                html += price + ' лв';
+                html += '</div>';
+            html += '</div>';
+        });
+        $('#content').append(html);
+
+    }
+});
+
